c3b36ae41dbd554042a20b288791643b1f55e3f0
Ron Alleva <ron.alleva@gmail.com>
2011-08-07 20:37:21 -0400

    Add some looping so that different words can be used, and also made it grow from the center.

--- src/com/alleva/views/WordsMainView.java
+++ src/com/alleva/views/WordsMainView.java
@@ -1,7 +1,10 @@
 package com.alleva.views;
 
 import android.content.Context;
+import android.content.SyncContext;
 import android.graphics.*;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
@@ -13,17 +16,19 @@ import android.widget.TextView;
 import com.alleva.R;
 
 import java.util.Random;
-import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 /**
  * User: ronnie
  * Date: 6/19/11
  */
-public class WordsMainView extends SurfaceView implements SurfaceHolder.Callback{
+public class WordsMainView extends SurfaceView implements SurfaceHolder.Callback {
 
     private Thread thread;
 
-    class WordThread extends Thread{
+    private Boolean newWord = new Boolean(false);
+
+    class WordThread extends Thread {
 
         private SurfaceHolder _surfaceHolder;
         private int numValue = 0;
@@ -33,48 +38,58 @@ public class WordsMainView extends SurfaceView implements SurfaceHolder.Callback
         private int xpos = 100;
         private int ypos = 200;
 
-        public WordThread(SurfaceHolder surfaceHolder){
+        public WordThread(SurfaceHolder surfaceHolder) {
             _surfaceHolder = surfaceHolder;
         }
 
         @Override
         public void run() {
 
-            Paint paint = new Paint();
-            Paint clearPaint = new Paint();
-            clearPaint.setColor(Color.BLACK);
-            clearPaint.setStyle(Paint.Style.FILL);
+
+            while (true) {
+
+                Paint paint = new Paint();
+                Paint clearPaint = new Paint();
+                clearPaint.setColor(Color.BLACK);
+                clearPaint.setStyle(Paint.Style.FILL);
 
 
-            paint.setAntiAlias(true);
-            paint.setTextSize(50);
-            paint.setColor(Color.DKGRAY);
-            paint.setAlpha(255);
-            String text =   "Some text";
+                paint.setAntiAlias(true);
+                paint.setTextSize(60);
+                paint.setColor(Color.DKGRAY);
+                paint.setAlpha(255);
+                String text = "Some text";
 
-            Rect bounds = new Rect();
+                Rect bounds = new Rect();
 
-            paint.getTextBounds(text, 0, text.length(), bounds);
+                paint.getTextBounds(text, 0, text.length(), bounds);
 
-            Bitmap bm = Bitmap.createBitmap(bounds.width(), bounds.height(), Bitmap.Config.ARGB_8888);
-            Canvas testCanvas = new Canvas(bm);
+                Bitmap bm = Bitmap.createBitmap(bounds.width(), bounds.height(), Bitmap.Config.ARGB_8888);
+                Canvas testCanvas = new Canvas(bm);
 
-            testCanvas.drawPaint(clearPaint);
-            testCanvas.drawText( text, 0, bounds.height() -1 , paint);
+                testCanvas.drawPaint(clearPaint);
+                testCanvas.drawText(text, 0, bounds.height() - 1, paint);
 
+                sizeY = 10;
+                sizeX = sizeY * (bounds.width() / bounds.height());
 
-            while (true){
-                Canvas c = null;
+                newWord = false;
+                messageHandler.postDelayed(newWordTask, 5000);
 
-                try {
-                    c = _surfaceHolder.lockCanvas(null);
-                    doDraw(c, bm);
-                    sleep(16);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-                } finally {
-                    if(c != null){
-                        _surfaceHolder.unlockCanvasAndPost(c);
+                while (true) {
+                    Canvas c = null;
+
+                    try {
+                        c = _surfaceHolder.lockCanvas(null);
+                        doDraw(c, bm);
+
+                        if (newWord) {
+                            break;
+                        }
+                    } finally {
+                        if (c != null) {
+                            _surfaceHolder.unlockCanvasAndPost(c);
+                        }
                     }
                 }
             }
@@ -87,18 +102,16 @@ public class WordsMainView extends SurfaceView implements SurfaceHolder.Callback
             clearPaint.setStyle(Paint.Style.FILL);
             c.drawPaint(clearPaint);
 
-            int realXPos = getPositionFromMidpoint(xpos, sizeX);
-            int realYPos = getPositionFromMidpoint(ypos, sizeY);
-            int realSizeX = (int)Math.floor(sizeX);
-            int realSizeY = (int)Math.floor(sizeY);
+            Rect rect = getRectangleFromDimensions(xpos, ypos, sizeX, sizeY);
 
-            c.drawBitmap(bm, null, new Rect(realXPos, realYPos, realSizeX, realSizeY), null);
+            c.drawBitmap(bm, null, rect, null);
+            drawSize(c);
 
-            sizeX = getNewSize(sizeX, 0.01);
-            sizeY = getNewSize(sizeY, 0.01);
+            sizeX = getNewSize(sizeX, 0.02);
+            sizeY = getNewSize(sizeY, 0.02);
 
             try {
-                wait(16);
+                wait(4);          // about 60 frames/sec
             } catch (InterruptedException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
@@ -106,17 +119,50 @@ public class WordsMainView extends SurfaceView implements SurfaceHolder.Callback
 
         }
 
+        private void drawSize(Canvas c) {
+            Paint paint = new Paint();
+
+            paint.setAntiAlias(true);
+            paint.setTextSize(8);
+            paint.setColor(Color.DKGRAY);
+            paint.setAlpha(255);
+
+
+            c.drawText("X:" + sizeX + ", " + "Y:" + sizeY, 20, 20, paint);
+        }
+
         private double getNewSize(double size, double scale) {
             return size + (size * scale);
         }
 
-        private int getPositionFromMidpoint(int centerPosition, double size) {
-            return (int)Math.floor(centerPosition - (size/2));
+        private Rect getRectangleFromDimensions(int centerX, int centerY, double width, double height) {
+            int left = (int) Math.floor(centerX - width / 2);
+            int right = (int) Math.floor(centerX + width / 2);
+            int top = (int) Math.floor(centerY - height / 2);
+            int bottom = (int) Math.floor(centerY + height / 2);
+
+            return new Rect(left, top, right, bottom);
         }
     }
 
+    class WordTimingThread extends Thread {
+
+        public void run() {
+            try {
+                sleep(5000);
+                newWord = true;
+            } catch (InterruptedException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+
+    }
+
+
     private TextView textView;
     private Animation mainAnimation;
+    private Handler messageHandler;
+    private Runnable newWordTask;
 
     public WordsMainView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -125,6 +171,14 @@ public class WordsMainView extends SurfaceView implements SurfaceHolder.Callback
         holder.addCallback(this);
 
         thread = new WordThread(getHolder());
+
+        messageHandler = new Handler();
+
+        newWordTask = new Runnable() {
+            public void run() {
+                newWord = true;
+            }
+        };
     }
 
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
@@ -138,7 +192,6 @@ public class WordsMainView extends SurfaceView implements SurfaceHolder.Callback
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
         //To change body of implemented methods use File | Settings | File Templates.
     }
-
 
 
 }
