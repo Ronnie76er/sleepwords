e1aa0516767b4d5bb5c79b485142b863eeca2a32
Ron Alleva <ron.alleva@gmail.com>
2011-12-04 18:32:16 -0500

    Add the launcher icon to the file

--- gen/com/alleva/R.java
+++ gen/com/alleva/R.java
@@ -25,7 +25,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int bed_icon=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int optionMenuButton=0x7f080002;
